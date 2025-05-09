c0c87f08de031967af46496f33aea627d7786739
zamuradsye <131319571+zamuradsye@users.noreply.github.com>
2025-05-09 12:26:44 +0500

    major updated's are added

--- pages/views.py
+++ pages/views.py
@@ -1,33 +1,20 @@
-# from django.shortcuts import render
-# from .models import Team
-# # Create your views here.
-# def home (request):
-#     team =  Team.objects.all()
-#     data = {
-#     'teams':team
-#     }
-#     return render(request, 'pages/home.html',data)
-
-# def about (request):
-#       team =  Team.objects.all()
-#     data = {
-#     'teams':team
-#     }
-#     return render(request, 'pages/about.html',data)
-
-# def services (request):
-#     return render(request,'pages/services.html')
-
-# def contact (request):
-#     return render(request, 'pages/contact.html')
-    
-
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html', {'teams': Team.objects.all()})
+    teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    all_cars = Car.objects.order_by('-created_date')
+    
+
+    context = {
+        'teams': teams,
+        'featured_cars': featured_cars,
+        'all_cars': all_cars,
+    }
+    return render(request, 'pages/home.html', context)
 
 def about(request):
     return render(request, 'pages/about.html', {'teams': Team.objects.all()})
